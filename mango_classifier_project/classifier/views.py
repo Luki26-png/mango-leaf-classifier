88e4ab7b296804c96382b27236ce98acbab68cef
Kuncoro Lukito <kuncorolukito36@gmail.com>
2025-06-10 12:41:39 +0700

    fix web

--- mango_classifier_project/classifier/views.py
+++ mango_classifier_project/classifier/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.conf import settings
 from skimage.feature import graycomatrix, graycoprops
-import albumentations as A
 from sklearn.preprocessing import LabelEncoder
 
 # HSV Segmentation
@@ -50,35 +49,7 @@ def predict(request):
         image = segment_leaf(image)
         image = cv2.resize(image, (128, 128))
 
-        # === Albumentation Augmentation ===
-        if algorithm == 'cnn':
-            transform = A.Compose([
-                A.Rotate(limit=30),
-                A.HorizontalFlip(p=0.5),
-                A.VerticalFlip(p=0.5),
-                A.RandomBrightnessContrast(p=0.5),
-                A.RandomScale(scale_limit=0.1, p=0.5),
-                A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5)
-            ])
-        elif algorithm == 'knn':
-            transform = A.Compose([
-                A.Rotate(limit=15, p=0.5),
-                A.HorizontalFlip(p=0.5),
-                A.VerticalFlip(p=0.3)
-            ])
-        elif algorithm == 'nb':
-            transform = A.Compose([
-                A.HorizontalFlip(p=0.5),
-                A.VerticalFlip(p=0.5)
-            ])
-        else:
-            return JsonResponse({'error': 'Invalid algorithm'}, status=400)
-
-        # Apply augmentation
-        augmented = transform(image=image)
-        image = augmented['image']
-
-        if algorithm in ['knn', 'nb']:
+        if algorithm in 'knn':
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             color_feat = extract_color_histogram(image)
             texture_feat = extract_haralick_features(gray)
@@ -88,7 +59,23 @@ def predict(request):
             reduced = pca.transform(combined)
 
             model = joblib.load(os.path.join(settings.MODELS_DATA_PATH, f"{algorithm}_model.pkl"))
-            le = joblib.load(os.path.join(settings.MODELS_DATA_PATH, 'label_encoder.pkl'))
+            le = joblib.load(os.path.join(settings.MODELS_DATA_PATH, f"{algorithm}_label_encoder.pkl"))
+
+            prediction = model.predict(reduced)[0]
+            label = le.inverse_transform([prediction])[0]
+        elif algorithm == 'nb':
+            feature = []
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            color_feat = extract_color_histogram(image)
+            texture_feat = extract_haralick_features(gray)
+            combined = np.concatenate((color_feat, texture_feat))
+            feature.append(combined)
+
+            pca = joblib.load(os.path.join(settings.MODELS_DATA_PATH, f"pca_{algorithm}_transform.pkl"))
+            reduced = pca.transform(combined.reshape(1, -1))
+
+            model = joblib.load(os.path.join(settings.MODELS_DATA_PATH, f"{algorithm}_model.pkl"))
+            le = joblib.load(os.path.join(settings.MODELS_DATA_PATH, f"{algorithm}_label_encoder.pkl"))
 
             prediction = model.predict(reduced)[0]
             label = le.inverse_transform([prediction])[0]
@@ -103,5 +90,7 @@ def predict(request):
             predicted_class = np.argmax(prediction)
             label = class_names[predicted_class]
 
+        else:
+            return JsonResponse({'error': 'Invalid algorithm'}, status=400)
 
         return JsonResponse({'result': label})
